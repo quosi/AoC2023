734c801c118064b7ee65325037169b035f979f7c
quosi <19479439+quosi@users.noreply.github.com>
2023-12-02 22:05:47 +0100

    setup for day3

---
+++ day3.py
@@ -0,0 +1,16 @@
+
+class Day3:
+
+    def __init__(self, data):
+        self.data = data
+
+    def day3_part1(self):
+        # with open(self.data) as fp:
+
+        return self.data
+
+
+if __name__ == '__main__':
+    data_ = 'data/day3_input.txt'
+    d3 = Day3(data_)
+    print(d3.day3_part1())
